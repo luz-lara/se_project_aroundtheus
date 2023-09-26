3fe606be2b4279fc61551d347fdf8e78812d1146
Luz Lara <luzalexandra.lara@gmail.com>
2023-09-26 10:27:25 -0400

    formatting code with prettier

--- scripts/index.js
+++ scripts/index.js
@@ -43,31 +43,30 @@ const profileAddButton = document.querySelector(".profile__add-button");
 const addCardCloseButton = addCardModal.querySelector(".modal__close-button");
 const cardListEl = document.querySelector(".gallery__list");
 const cardTemplate =
-document.querySelector("#card-template").content.firstElementChild;
-//preview image modal 
-const previewImageModal=document.querySelector("#preview-image-modal");
-const previewCloseButton=document.querySelector(".preview__close-button");
-//form data 
-const cardTitle=addCardModal.querySelector(".modal__input-title");
-const cardLink= addCardModal.querySelector(".modal__input-url");
-const addCardForm=document.forms["add-card-form"];
- //functions
-function openModal(modal){
+  document.querySelector("#card-template").content.firstElementChild;
+//preview image modal
+const previewImageModal = document.querySelector("#preview-image-modal");
+const previewCloseButton = document.querySelector(".preview__close-button");
+//form data
+const cardTitle = addCardModal.querySelector(".modal__input-title");
+const cardLink = addCardModal.querySelector(".modal__input-url");
+const addCardForm = document.forms["add-card-form"];
+//functions
+function openModal(modal) {
   modal.classList.add("modal_opened");
-};
+}
 function closePopup(modal) {
   modal.classList.remove("modal_opened");
-};
-function renderCard(cardData,cardListEl){
-  const cardElement=getCardElement(cardData);
-  cardListEl.prepend(cardElement);
-
 }
-function handleAddCardFormSubmit(evt){
+function renderCard(cardData, cardListEl) {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+}
+function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
-  const name= cardTitle.value;
-  const link= cardLink.value;
-  renderCard({name,link},cardListEl);
+  const name = cardTitle.value;
+  const link = cardLink.value;
+  renderCard({ name, link }, cardListEl);
   closePopup(addCardModal);
 }
 //event listeners click
@@ -77,16 +76,18 @@ profileEditButton.addEventListener("click", function () {
   openModal(profileModal);
 });
 
-profileAddButton.addEventListener("click",()=>{
+profileAddButton.addEventListener("click", () => {
   openModal(addCardModal);
 });
 
-closeEditButton.addEventListener("click", ()=> {
+closeEditButton.addEventListener("click", () => {
   closePopup(profileModal);
 });
-addCardCloseButton.addEventListener("click", ()=> closePopup(addCardModal));
+addCardCloseButton.addEventListener("click", () => closePopup(addCardModal));
 
-previewCloseButton.addEventListener("click",()=>closePopup(previewImageModal));
+previewCloseButton.addEventListener("click", () =>
+  closePopup(previewImageModal)
+);
 //event listeners submit
 profileForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -94,30 +95,30 @@ profileForm.addEventListener("submit", (e) => {
   profileDescription.textContent = profileDescriptionInput.value;
   closePopup(profileModal);
 });
-addCardForm.addEventListener("submit",handleAddCardFormSubmit);
+addCardForm.addEventListener("submit", handleAddCardFormSubmit);
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardTitle = cardElement.querySelector(".card__title");
   const cardImageEl = cardElement.querySelector(".card__image");
-  const likeButton=cardElement.querySelector(".card__like-button");
-  const deleteButton=cardElement.querySelector(".card__delete-button")
-  const previewModalTitle=document.querySelector(".preview__title")
-  const previewImage=document.querySelector(".modal__preview-image");
-  
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const previewModalTitle = document.querySelector(".preview__title");
+  const previewImage = document.querySelector(".modal__preview-image");
+
   //event listeners
-  likeButton.addEventListener("click",() =>{
-  likeButton.classList.toggle("card__like-button_active")});
-  
-  deleteButton.addEventListener("click",() =>{
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  deleteButton.addEventListener("click", () => {
     cardElement.remove();
-  })
-  cardImageEl.addEventListener("click",() =>{ 
-    previewImage.src=cardData.link;
-    previewModalTitle.textContent=cardData.name;
+  });
+  cardImageEl.addEventListener("click", () => {
+    previewImage.src = cardData.link;
+    previewModalTitle.textContent = cardData.name;
     openModal(previewImageModal);
-   
-  })
+  });
 
   cardTitle.textContent = cardData.name;
   cardImageEl.src = cardData.link;
