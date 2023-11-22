a0c4f3b224d6708f73259b16fa0d0abea1a14612
luz-lara <136045689+luz-lara@users.noreply.github.com>
2023-11-22 17:28:02 -0500

    added enablevalidation and eventlistener function

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,9 +1,27 @@
-function enableValidation(config ){
-    console.log(config); 
-    
+function setEventListeners(formElement,options){
+
 }
+
+function enableValidation(options ){
+const formEls=[...document.querySelectorAll(options.formSelector)];
+formEls.forEach((formEls) => {
+    formEls.addEventListener('submit',(e) => {
+        e.preventDefault();
+    });
+    
+    //look for all inputs inside of the form 
+    //loop through all the inputs to see if all are valid 
+    //if input is NOT valid - grab validation message
+    // - add error class to input 
+    //display error message 
+    //disable button 
+    // if all inputs are valid 
+    //- enable button 
+    //reset error messages 
+});
+};
 const config={
-    formSelector: ".popup__form",
+    formSelector: ".modal__form",
     inputSelector: ".popup__input",
     submitButtonSelector: ".popup__button",
     inactiveButtonClass: "popup__button_disabled",
@@ -12,4 +30,4 @@ const config={
 }
 
 
-enableValidation(config); 
+enableValidation(config);
