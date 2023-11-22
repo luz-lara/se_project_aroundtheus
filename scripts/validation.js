a91c75dbb046e902a31d2961f2db553b9db55126
luz-lara <136045689+luz-lara@users.noreply.github.com>
2023-11-22 12:15:23 -0500

    added validation sheet and config object

---
+++ scripts/validation.js
@@ -0,0 +1,15 @@
+function enableValidation(config ){
+    console.log(config); 
+    
+}
+const config={
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible"
+}
+
+
+enableValidation(config); 
