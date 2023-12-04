9f274ac763a60c59ea890838c7b991a1c55d5ca5
luz-lara <136045689+luz-lara@users.noreply.github.com>
2023-12-03 23:30:51 -0500

    fixed error to enable button modal modifier

--- scripts/validation.js
+++ scripts/validation.js
@@ -70,7 +70,7 @@ const config={
     formSelector: ".modal__form",
     inputSelector: ".modal__input",
     submitButtonSelector: ".modal__button",
-    inactiveButtonClass: ".modal__button_disabled",
+    inactiveButtonClass: "modal__button_disabled",
     inputErrorClass: ".modal__input_type_error",
     errorClass: ".modal__error_visible"
 };
