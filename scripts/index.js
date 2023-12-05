3e62e4adb415e44373dfdef4919a5cf6f9c8f4d0
luz-lara <136045689+luz-lara@users.noreply.github.com>
2023-12-05 16:58:24 -0500

    keyhandler object

--- scripts/index.js
+++ scripts/index.js
@@ -53,8 +53,8 @@ const addCardForm = document.forms["add-card-form"];
 //functions
 function keyHandler (e){
 if (e.key === "Escape"){
-  const openModal=document.querySelector(".modal__opened");
-  closePopup(openModal);
+  const modal=document.querySelector(".modal__opened");
+  closePopup(modal);
 }
 }
 function openModal(modal) {
