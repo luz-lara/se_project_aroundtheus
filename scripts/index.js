1b66aa499e24d90f75c91b4e1263ca06926b2297
luz-lara <136045689+luz-lara@users.noreply.github.com>
2023-12-08 14:42:37 -0500

    updated Handle escape function

--- scripts/index.js
+++ scripts/index.js
@@ -56,6 +56,7 @@ const addCardForm = document.forms["add-card-form"];
 //functions
 function handleEscape(e) {
   if (e.key === "Escape" || e.key === "Esc") {
+    const modal=document.querySelector(".modal_opened"); 
     closePopup(modal);
   }
 }
